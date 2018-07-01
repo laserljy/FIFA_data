45be4265b5de8f40862b97fbf51cac7b8a74b338
laserljy <lijiyinglaser@gmail.com>
2018-07-01 10:44:00 -0700

    Add files via upload

--- testProgram/test4.cpp
+++ testProgram/test4.cpp
@@ -30,6 +30,14 @@ class Game{
 		return os;
 	}
 
+	friend void addToMap (map<int, int>& gameID2Year, const Game& g){
+		if(gameID2Year.find(g.gameID)==gameID2Year.end()) gameID2Year.insert(pair<int, int>(g.gameID, g.year));
+	}
+
+	// friend void print(const Game& g){
+	// 	cout << "Game year: " << g.year << endl;
+	// }
+
 	vector<string> str2Vec(string input){
 		vector<string> result;
 		// A bug was found when trying to use country name abbreviation instead of full name. Then the print out formation is quite confusing, after trying different elements, I finally identitified that it is because we access the last elem in the vector.
@@ -60,32 +68,49 @@ class Game{
 	}
 };
 
+// void print(const Game& g);
+void addToMap(map<int, int>& gameID2Year, const Game& g);
+
 int main(int argc, char** argv){
+	// build the map for games. <year, games in that year>
 	map<int, vector<Game*>> games;
+	map<int, int> gameID2Year;
 	ifstream file(argv[1]);
 	string line;
 	getline(file, line);
 	while(getline(file,line)){
 		Game* game = new Game(line);
+		addToMap(gameID2Year, *game);
+		//print(*game);
 		int year = game->getYear();
 		if(games.find(year)==games.end()) {
 			games.insert(pair<int, vector<Game*>>(year,vector<Game*>(0)));
 		}
 		games[year].push_back(game);
 	}
-	int count;
-	for(auto yearItr=games.begin(); yearItr!=games.end(); ++ yearItr){
-		count = 0;
-		int year = yearItr->first;
-		auto allGames = yearItr->second;
-		cout << "=========" << year << "=========" << endl; 
+
+	// Print games to check.
+	// int count;
+	// for(auto yearItr=games.begin(); yearItr!=games.end(); ++ yearItr){
+	// 	count = 0;
+	// 	int year = yearItr->first;
+	// 	auto allGames = yearItr->second;
+	// 	cout << "=========" << year << "=========" << endl; 
 		
-		for(auto gameItr=allGames.begin(); gameItr!=allGames.end(); ++gameItr){
-			cout << **gameItr << endl;
-			count++;
-		}
-		cout << "Total game for " << year << ": " << count << endl;
+	// 	for(auto gameItr=allGames.begin(); gameItr!=allGames.end(); ++gameItr){
+	// 		cout << **gameItr << endl;
+	// 		count++;
+	// 	}
+	// 	cout << "Total game for " << year << ": " << count << endl;
+	// }
+
+	// ============
+
+	// Print gameID2Year to check.
+	for(auto iter = gameID2Year.begin(); iter!=gameID2Year.end(); ++iter){
+		cout << "GameID: " << iter->first << ", Year: " << iter->second << endl; 
 	}
+
 
 	for(auto yearItr=games.begin(); yearItr!=games.end(); ++ yearItr){
 		// int year = yearItr->first;
